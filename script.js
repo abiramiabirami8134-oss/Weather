c91f93c6f4502bd8659ecba815a2148a49300e37
abiramiabirami8134-oss <abiramiabirami8134@gmail.com>
2026-03-05 20:15:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// Function to show a message
+function showMessage() {
+    alert("Hello! Welcome to my website.");
+}
+
+// Change text of an element
+function changeText() {
+    document.getElementById("demo").innerHTML = "Text has been changed!";
+}
+
+// Console message
+console.log("JavaScript file connected successfully!");
